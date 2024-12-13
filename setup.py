969e0fa6707177e28734c79d0d568e81bf294f41
Benji Huser-Berta <benj.huser@gmail.com>
2024-12-13 16:41:00 +0100

    Bump version to 1.0.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flowpulse',
-    version='1.0.11',
+    version='1.0.12',
     packages=find_packages(),
     include_package_data=True,
     package_data={
